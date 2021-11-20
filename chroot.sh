e1bf23be34a89c3e8940a6d621dbd6646cbc21a5
Keith Mathiesen <keithmathiesen@gmail.com>
2021-11-19 19:42:27 -1000

    Changes to be committed: 	modified:   chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -78,6 +78,6 @@ zfs allow -u ${myUser} mount,snapshot,destroy $(df --output=source /home | tail 
 chown -R ${myUser}:${myUser} /home/${myUser}
 chmod 700 /home/${myUser}
 passwd ${myUser}
-
+usermod -aG audio,video,optical,storage,network,wheel
 # Leave chroot
 #exit
